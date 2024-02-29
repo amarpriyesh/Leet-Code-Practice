61aeb8ab5df3e3b42e84afb5b524652226fe3a56
Fnu Priyesh <63479437+amarpriyesh@users.noreply.github.com>
2024-02-29 04:53:34 -0500

    Time: 40 ms (34.38%), Space: 16.5 MB (84.96%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -4,10 +4,17 @@ class Solution:
         i = 0
         div = 1
         res = 0
+        fac = []
+        rev = []
+        if n == 1:
+            return 1
         
-        while i <= k and ( div <= n):
+        while i <= k and ( div ** 2 <= n):
             if n % div == 0:
                 i += 1
+                fac.append(div)
+                if n // div != div:
+                    rev.append(n // div)
                 if i == k:
                     break
             div += 1
@@ -19,5 +26,17 @@ class Solution:
         
         if i == k:
             return  div
+        rev.sort()
+        fac.extend(rev)
+        print(fac)
+        
+        if (len(fac)) >= k:
+           
+            return fac[k - 1]
+        
+        print(fac)
+            
+            
+            
         return -1
         

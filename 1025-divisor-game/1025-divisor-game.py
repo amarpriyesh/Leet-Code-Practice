4cef6c1453b5136a22fb36f23e01e6942c703219
Fnu Priyesh <63479437+amarpriyesh@users.noreply.github.com>
2024-03-01 07:41:15 -0500

    Time: 30 ms (87.23%), Space: 16.7 MB (41.38%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,5 +1,9 @@
 class Solution:
+    mem = {}
     def divisorGame(self, n: int) -> bool:
+        
+        if n in self.mem.keys():
+            return self.mem[n]
         
         if n <= 1:
             return False
@@ -8,7 +12,8 @@ class Solution:
         
         for i in range(1, n):
             if (n % i) == 0:
-                return not self.divisorGame(n - i)
+                self.mem[n] = not self.divisorGame(n - i)
+                return self.mem[n]
             
         
                 

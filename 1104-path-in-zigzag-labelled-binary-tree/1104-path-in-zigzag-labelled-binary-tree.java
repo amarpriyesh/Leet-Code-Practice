43907386f7c0fa13bac06c24b09c413cba3e69a6
Fnu Priyesh <63479437+amarpriyesh@users.noreply.github.com>
2024-03-03 10:09:30 -0500

    Time: 4 ms (7.69%), Space: 40.7 MB (81.03%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> pathInZigZagTree(int label) {
+        
+       
+        
+        List<Integer> in = new ArrayList<>();
+        
+       
+        int level = level(label);
+        int pos;
+        if(level % 2 != 0) {
+            pos = label;
+        }
+        else {
+            pos = (int)Math.pow(2, level - 1) * 3 - label - 1 ;
+        }
+        
+        while (pos != 1) {
+            System.out.println(pos);
+            in.add(label);
+            int child = (int) pos / 2;
+            level --;
+             if(level % 2 != 0) {
+            label = child;
+        }
+        else {
+            label = (int)Math.pow(2, level - 1) * 3 - child - 1 ;
+        }
+           pos = child;
+          
+            
+            
+            
+        }
+        in.add(1);
+        Collections.sort(in);
+        
+        return in;
+        
+        
+    }
+    
+    public int level(int k) {
+        int j = 1;
+         while(Math.pow(2,j) <= k) {
+            j++;
+        }
+        
+        return j;
+    }
+}
